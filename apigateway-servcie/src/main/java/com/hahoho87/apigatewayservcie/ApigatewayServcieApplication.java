c93b6bf0e0982073596b3e706b053b983bb9fb49
bky4564 <bky4564@hunet.co.kr>
2022-01-17 12:34:15 +0900

    add api-gateway-service module

---
+++ apigateway-servcie/src/main/java/com/hahoho87/apigatewayservcie/ApigatewayServcieApplication.java
@@ -0,0 +1,13 @@
+package com.hahoho87.apigatewayservcie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayServcieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayServcieApplication.class, args);
+    }
+
+}
---
+++ apigateway-servcie/src/test/java/com/hahoho87/apigatewayservcie/ApigatewayServcieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hahoho87.apigatewayservcie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServcieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
