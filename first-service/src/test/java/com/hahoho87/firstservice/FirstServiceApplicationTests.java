b96eda875f2ddaea596b35ffa6d6c7d85e9149ef
bky4564 <bky4564@hunet.co.kr>
2022-01-17 13:13:46 +0900

    add first-service for test api gateway

---
+++ first-service/src/test/java/com/hahoho87/firstservice/FirstServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hahoho87.firstservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
