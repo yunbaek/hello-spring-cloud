06dca654fe5ea0668302a4b70a077f4c96179874
bky4564 <bky4564@hunet.co.kr>
2022-01-17 12:03:36 +0900

    add discovery-server module

---
+++ discovery-service/src/test/java/com/hahoho87/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hahoho87.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
