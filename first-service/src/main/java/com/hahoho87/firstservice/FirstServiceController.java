a9e3bf413217efbd2a43648712ccc193de027a4a
hahoho87 <bbaing4564@gmail.com>
2022-01-18 00:04:45 +0900

    java config 를 통한 gateway filter 적용

--- first-service/src/main/java/com/hahoho87/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/hahoho87/firstservice/FirstServiceController.java
@@ -1,15 +1,24 @@
 package com.hahoho87.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/first-service")
+@Slf4j
 public class FirstServiceController {
 
-    @GetMapping("/first-service/welcome")
+    @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First Service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info("header = {}", header);
+        return "Hello World in First Service.";
     }
 }
--- second-service/src/main/java/com/hahoho87/secondservice/SecondController.java
+++ second-service/src/main/java/com/hahoho87/secondservice/SecondController.java
@@ -1,14 +1,25 @@
 package com.hahoho87.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/second-service")
+@Slf4j
 public class SecondController {
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Second Service.";
     }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info("header = {}", header);
+        return "Hello World in Second Service.";
+    }
+
 }
