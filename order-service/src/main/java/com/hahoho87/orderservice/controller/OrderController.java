114288e44e8bc75ef594999a20fe5e67795b54ee
bky4564 <bky4564@hunet.co.kr>
2022-08-02 13:35:14 +0900

    [feat] apply database sync using kafka

--- order-service/src/main/java/com/hahoho87/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/hahoho87/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.hahoho87.orderservice.controller;
 import com.hahoho87.orderservice.dto.OrderDto;
 import com.hahoho87.orderservice.entity.OrderEntity;
 import com.hahoho87.orderservice.messagequeue.KafkaProducer;
+import com.hahoho87.orderservice.messagequeue.OrderProducer;
 import com.hahoho87.orderservice.service.OrderService;
 import com.hahoho87.orderservice.vo.OrderRequest;
 import com.hahoho87.orderservice.vo.OrderResponse;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
@@ -21,11 +23,14 @@ public class OrderController {
     private final OrderService orderService;
     private final ModelMapper mapper;
     private final KafkaProducer kafkaProducer;
+    private final OrderProducer orderProducer;
 
-    public OrderController(OrderService orderService, ModelMapper mapper, KafkaProducer kafkaProducer) {
+    public OrderController(OrderService orderService, ModelMapper mapper,
+                           KafkaProducer kafkaProducer, OrderProducer orderProducer) {
         this.orderService = orderService;
         this.mapper = mapper;
         this.kafkaProducer = kafkaProducer;
+        this.orderProducer = orderProducer;
     }
 
     @GetMapping("/health-check")
@@ -38,12 +43,16 @@ public class OrderController {
                                                      @PathVariable String userId) {
         OrderDto orderDto = getMap(order, OrderDto.class);
         orderDto.setUserId(userId);
-        OrderDto createdOrder = orderService.createOrder(orderDto);
+
+        /* kafka */
+        orderDto.setOrderId(UUID.randomUUID().toString());
+        orderDto.setTotalPrice(this.getTotalPrice(orderDto));
 
         /* send order to the kafka */
         kafkaProducer.send("example-catalog-topic", orderDto);
+        orderProducer.send("orders", orderDto);
 
-        OrderResponse result = getMap(createdOrder, OrderResponse.class);
+        OrderResponse result = getMap(orderDto, OrderResponse.class);
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 
@@ -59,4 +68,9 @@ public class OrderController {
     public <D> D getMap(Object source, Class<D> destinationType) {
         return mapper.map(source, destinationType);
     }
+
+    private int getTotalPrice(OrderDto dto) {
+        return dto.getUnitPrice() * dto.getQty();
+    }
+
 }
---
+++ order-service/src/main/java/com/hahoho87/orderservice/dto/KafkaOrderDto.java
@@ -0,0 +1,13 @@
+package com.hahoho87.orderservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class KafkaOrderDto implements Serializable {
+    private Schema schema;
+    private Payload payload;
+}
